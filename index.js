c31f7565b4d007aeb7b2ea638f6379d7a73dc26d
vaibhavbandal <86363586+vaibhavbandal@users.noreply.github.com>
2022-11-06 14:52:52 +0530

    added test route

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv");
-dotenv.config({
-    path : "./configuration/config.env"
-});
+dotenv.config();
+
 const database = require("./configuration/databaseConfig");
 const corsOptions = {
     exposedHeaders: 'Authorization',
@@ -21,6 +20,10 @@ app.use('/api/v1/admin',require('./routes/admin'));
 app.use('/api/v1/librarian',require('./routes/librarian'));
 app.use('/api/v1/student',require('./routes/student'));
 app.use('/api/v1/common',require('./routes/common'));
+
+app.get('test',(req,res)=>{
+    res.send('testing done!');
+})
 
 database()
     .then(()=>console.log("Connected To Database"))
